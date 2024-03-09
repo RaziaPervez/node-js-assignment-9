c5a50cc1aafd136fa20981074874d560f6f564a9
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 09:56:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//project=9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+var favnum = 15;
+var message = "my favorites number is:";
+console.log(message, favnum);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//project=9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+let favnum = 15;
+let message = "my favorites number is:";
+console.log (message,favnum)
